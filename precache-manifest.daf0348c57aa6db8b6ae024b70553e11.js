c98fe8fcb34594d5b4dfdd57a0cf339adb1b5bda
Robin Das <dassrobin77@gmail.com>
2021-09-20 09:21:40 +0530

    Updates

---
+++ precache-manifest.daf0348c57aa6db8b6ae024b70553e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037dd3bedd5aae769d9729feaf3ed59e",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "275ccc5df96e553f53d9",
+    "url": "/react/static/css/main.2ded68fc.chunk.css"
+  },
+  {
+    "revision": "19a02f0edfd296cac57c",
+    "url": "/react/static/js/2.0c67da29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.0c67da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275ccc5df96e553f53d9",
+    "url": "/react/static/js/main.572248dd.chunk.js"
+  },
+  {
+    "revision": "b5961b11ef0fbffcdc21",
+    "url": "/react/static/js/runtime-main.fe144a6d.js"
+  }
+]);
